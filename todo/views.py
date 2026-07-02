b6cd7b2181ea0fbb1564b562dbb841beca6f4b44
1f10250297 <s1f102502979@iniad.org>
2026-07-02 13:11:39 +0900

    修正3

--- todo/views.py
+++ todo/views.py
@@ -45,10 +45,11 @@ def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
-        if request.method == 'POST':
-            task.title = request.POST['title']
-            task.due_at = make_aware(parse_datetime(request.POST['due_at']))
-            task.save()
+        raise Http404('Task does not exist')
+    if request.method == 'POST':
+        task.title = request.POST['title']
+        task.due_at = make_aware(parse_datetime(request.POST['due_at']))
+        task.save()
         return redirect('detail', task_id=task.pk)
     context = {
         'task': task,
